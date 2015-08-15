cjkacz/proprietary_vendor_moto

more of the radio stack debug apps and dmagent
This along with the previous commit adds support for things like the OEM Debug Menus, the sprint specific hidden menu, lifetime data menu(lets you see how long the phone has been used, wheter it has been recertified or not) this also supports things like OEM radio access, NVinfo, Oem connection monitoring, radio config, OemAccessManager, Diagnosis monitoring, This also seems to help the GoogleConnectivity service as well as a number of carrier related issues

This also makes the rom compatible with Google Apps Device Policy software from the play store https://play.google.com/store/apps/details?id=com.google.android.apps.enterprise.dmagent&hl=en

Warning: make sure you have disable the update components in your rom https://github.com/PureNexus/android_vendor_nexus/commit/0e087aebbd0482bdff70358c85852c9e08de65db

Tested and Compiled For The Pure Nexus Project By @BeansTown106

shamu/shamu-vendor-blobs.mk
@@ -390,6 +390,7 @@ PRODUCT_COPY_FILES += \
     vendor/moto/shamu/proprietary/bin/usbhub_init:system/bin/usbhub_init \
     vendor/moto/shamu/proprietary/etc/permissions/com.motorola.DirectedSMSProxy.xml:system/etc/permissions/com.motorola.DirectedSMSProxy.xml \
     vendor/moto/shamu/proprietary/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/moto/shamu/proprietary/etc/permissions/com.android.omadm.service.xml:system/etc/permissions/com.android.omadm.service.xml \
     vendor/moto/shamu/proprietary/vendor/firmware/venus.b00:system/vendor/firmware/venus.b00 \
     vendor/moto/shamu/proprietary/vendor/firmware/venus.b01:system/vendor/firmware/venus.b01 \
     vendor/moto/shamu/proprietary/vendor/firmware/venus.b02:system/vendor/firmware/venus.b02 \

andrewwrightt/vendor_motorola_shamu

Conflicts:
	shamu/shamu-vendor-blobs.mk
Change-Id: Id04ff81c28ca507efda4e8a74bfb9dad81c9d3f3

shamu-vendor-blobs.mk
@@ -380,6 +380,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/shamu/proprietary/bin/usbhub_init:system/bin/usbhub_init \
     vendor/motorola/shamu/proprietary/etc/permissions/com.motorola.DirectedSMSProxy.xml:system/etc/permissions/com.motorola.DirectedSMSProxy.xml \
     vendor/motorola/shamu/proprietary/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/motorola/shamu/proprietary/etc/permissions/com.android.omadm.service.xml:system/etc/permissions/com.android.omadm.service.xml \
     vendor/motorola/shamu/proprietary/vendor/firmware/venus.b00:system/vendor/firmware/venus.b00 \
     vendor/motorola/shamu/proprietary/vendor/firmware/venus.b01:system/vendor/firmware/venus.b01 \
     vendor/motorola/shamu/proprietary/vendor/firmware/venus.b02:system/vendor/firmware/venus.b02 \